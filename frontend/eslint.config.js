6b158d1753108905f0bce403f82ccf5f5acf4f43
Aharshi3614 <aharshisinha2020@gmail.com>
2026-06-30 20:16:32 +0530

    feat: Add Storybook and Chromatic for visual regression testing

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -6,23 +9,20 @@ import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-    },
+export default tseslint.config({ ignores: ["dist"] }, {
+  extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+  files: ["**/*.{ts,tsx}"],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
   },
-);
+  plugins: {
+    "react-hooks": reactHooks,
+    "react-refresh": reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+  },
+}, storybook.configs["flat/recommended"]);
 
